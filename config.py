542406a3bd35b91204ddb530588749463fe28679
Jeff <Xavierjeff451@gmail.com>
2022-05-16 01:06:39 +0300

    set configurations for the application

---
+++ config.py
@@ -0,0 +1,40 @@
+import os
+from sqlalchemy import create_engine, exc
+
+
+class Config:
+    '''
+    General configuration parent class
+    
+    '''
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://paulineapondi:1989@localhost/blog'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+        SQLALCHEMY_DATABASE_URI =os.environ.get("DATABASE_URL").replace("://", "ql://", 1)
+    '''
+    
+    
+    
+    
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+    DEBUG = True
+    
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+
+}
