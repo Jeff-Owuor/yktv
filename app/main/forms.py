4bb25680d1263d3d2058fe1ca9837a13414bf5d7
Jeff <Xavierjeff451@gmail.com>
2022-05-16 16:01:28 +0300

    user profile

--- app/main/forms.py
+++ app/main/forms.py
@@ -2,6 +2,11 @@ from flask_wtf import FlaskForm
 from wtforms import TextAreaField,SubmitField, StringField
 from wtforms.validators import DataRequired
 
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell us about you.',validators = [DataRequired()])
+    submit = SubmitField('Submit')
+    
+
 class BlogForm(FlaskForm):
     title = StringField('Title',validators=[DataRequired()])
     blog = TextAreaField('Blog', validators=[DataRequired()])
