5d70b3d39f0238b75851af222527d14046cd3595
Jeff <Xavierjeff451@gmail.com>
2022-05-17 07:25:08 +0300

    user tests

--- config.py
+++ config.py
@@ -12,6 +12,10 @@ class Config:
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://paulineapondi:1989@localhost/blog'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOADED_PHOTOS_DEST ='app/static/photos'
+    
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://james:password@localhost/blog_test'
+    
 class ProdConfig(Config):
     '''
     Production  configuration child class
@@ -31,11 +35,11 @@ class DevConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    pass
     DEBUG = True
     
 config_options = {
 'development':DevConfig,
 'production':ProdConfig,
+'test':TestConfig
 
 }
---
+++ tests/test_user.py
@@ -0,0 +1,37 @@
+import unittest
+from app.models import User,db;
+
+class UserModelTest(unittest.TestCase):
+    def setUp(self):
+        self.new_user = User(username = "Oparanya", email ="oparanya@gmail.com", bio = "Freaky", profile_pic_path = "static/photos", password = '1234567')
+        db.session.add(self.new_user)
+        db.session.commit()
+
+    def tearDown(self):
+        User.query.delete()
+        db.session.commit()
+ 
+    def test_password_setter(self):
+        self.assertTrue(self.new_user.pass_secure is not None)
+
+    def test_password_verification(self):
+        self.assertTrue(self.new_user.verify_password('1234567'))
+
+    def test_save_user(self):
+        self.new_user.save_user()
+        self.assertTrue(len(User.query.all())>0)
+
+    def test_check_instance_variables(self):
+        self.assertEquals(self.new_user.username, 'Oparanya')
+        self.assertEquals(self.new_user.email, 'Oparanya@gmail.com')
+        self.assertEquals(self.new_user.bio, 'freaky')
+        self.assertEquals(self.new_user.profile_pic_path, 'static/photos')
+        self.assertTrue(self.new_user.verify_password('12345'))
+
+
+    def test_no_access_password(self):
+        with self.assertRaises(AttributeError):
+            self.new_user.password 
+            
+if __name__ == '__main':
+    unittest.main()            
